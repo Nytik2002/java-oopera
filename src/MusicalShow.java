5ee082982ba73dba84653fa03237cc42210530c9
AAA <dsadjk@gmail.com>
2026-03-18 01:27:43 +0500

    Okay, lets go :)

--- src/MusicalShow.java
+++ src/MusicalShow.java
@@ -1,8 +1,8 @@
 public class MusicalShow extends Show {
-    String musicAuthor; //автор музыки
-    String librettoText; //текст либретто
+    Person musicAuthor; //автор музыки
+    protected String librettoText; //текст либретто
 
-    public MusicalShow(String title, int duration, Director director, String musicAuthor, String librettoText) {
+    public MusicalShow(String title, int duration, Director director, Person musicAuthor, String librettoText) {
         super(title, duration, director);
         this.musicAuthor = musicAuthor;
         this.librettoText = librettoText;

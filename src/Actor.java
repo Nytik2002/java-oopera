9a2994746dd28f3b76a36321d3340dbedf858993
AAA <dsadjk@gmail.com>
2026-03-17 19:12:52 +0500

    F

---
+++ src/Actor.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Objects;
+public class Actor extends Person {
+    public int height;
+    public Actor (String name, String surname, Gender gender, int height) {
+        super(name, surname, gender);
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", " + surname + ", " + "(" + height + ")";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Actor actor = (Actor) obj;
+        return height == actor.height && name.equals(actor.name) && surname.equals(actor.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, height);
+    }
+
+}
